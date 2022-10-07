b6b62cffc85908f716462f86fe80f8074499b2c4
yusuffikry <113654172+yusuffikry@users.noreply.github.com>
2022-10-07 10:31:59 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+n = int(input('Bilangan : '))
+
+def factorial(n):
+    if n < 0:
+        print("tidak terdefinisi")
+    elif n > 1:
+        return n*factorial(n-1)
+    else:
+        return 1
+print(factorial(n))

a1cc38b3b93138bcf9950ae700dc5ebc046a4af6
yusuffikry <113654172+yusuffikry@users.noreply.github.com>
2022-10-18 23:03:49 +0800

    Delete Tugas Pekan I directory

--- Tugas Pekan I/3.py
+++
@@ -1,9 +0,0 @@
-#Nama seller, gaji pokok, dan total penjualan dalam bulan itu.
-
-a = str(input("masukkan nama pertama seller : "))
-b = float(input("masukkan gaji pokok : "))
-c = float(input("masukkan total penjualan seller : "))
-
-total = float(b + c *15/100)
-
-print ("total = $ " , round (total,2))
